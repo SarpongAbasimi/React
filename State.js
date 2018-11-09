a027659095b13fc23fbdd8c8c7d81ce8ec7d8c96
sarpong <abasimisarpong@rocketmail.com>
2018-11-09 17:31:08 +0000

    React(Internal Component State)

---
+++ State.js
@@ -0,0 +1,49 @@
+
+import   React  from 'react';
+import  ReactDOM  from 'react-dom'
+
+const list = [
+    {
+      title: 'React',
+      url: 'https://facebook.github.io/react/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+  }, 
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+}, 
+];
+
+const MyApp = class SayHello extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            list:list,
+        }
+    }
+
+    render() {
+        return(
+            <div>
+                {this.state.list.map(elements=>
+                    <div keys={elements.objectID}>
+                        <span>{elements.author}</span>
+                    </div>
+
+
+                )}
+            </div>
+
+        );
+    }
+
+}
+
+ReactDOM.render(<MyApp/>,document.getElementById("root")) 
